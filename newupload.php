0e79b52207c0818ec98c618cc2ef13470a72b241
skych1 <sc4rdz@virginia.edu>
2022-03-15 17:28:16 -0400

    php

---
+++ newupload.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">  
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="your name">
+        <meta name="description" content="include some description about your page">  
+        <title>Form Example Submission Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
+    </head>
+
+    <body>
+        <div class="container" style="margin-top: 15px;">
+            <div class="row col-xs-12">
+                <h1>Example Form Submission</h1>
+            </div>
+           <div class="row col-xs-12">
+                <pre>
+                    <?php
+                        // Look at the POST array
+                        print_r($_POST);
+                        // More information about the POST variable
+                        var_dump($_POST);
+                    ?>
+                </pre>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </body>
+</html>
