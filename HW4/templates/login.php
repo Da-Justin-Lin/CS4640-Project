ed5e693c91b37d2f18daa482476ba668a09ee4a1
skych1 <sc4rdz@virginia.edu>
2022-03-22 16:29:12 -0400

    fix

--- HW4/templates/login.php
+++ HW4/templates/login.php
@@ -3,16 +3,16 @@
     <head>
         <meta charset="UTF-8">  
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="CS4640">
-        <meta name="description" content="CS4640 Trivia Login Page">  
-        <title>Trivia Game Login</title>
+        <meta name="author" content="Sky Chen; Da Lin">
+        <meta name="description" content="Wordle Extreme Login Page">  
+        <title>Wordle Extreme Login</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
     </head>
     <body>
         <div class="container" style="margin-top: 15px;">
             <div class="row col-xs-8">
-                <h1>CS4640 Trivia Game - Get Started</h1>
-                <p> Welcome to our trivia game!  To get started, enter a username and password.</p>
+                <h1>Wordle Extreme - Get Started</h1>
+                <p> Welcome to our Wordle Extreme game  To get started, enter a username and email.</p>
             </div>
             <div class="row justify-content-center">
                 <div class="col-4">
@@ -30,12 +30,8 @@
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name"/>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password"/>
-                    </div>
                     <div class="text-center">                
-                    <button type="submit" class="btn btn-primary">Play Trivia</button>
+                    <button type="submit" class="btn btn-primary">Play Wordle Extreme</button>
                     </div>
                 </form>
                 </div>
--- HW4/templates/question.php
+++ HW4/templates/question.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">  
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="your name">
+        <meta name="author" content="Sky Chen; Da Lin">
         <meta name="description" content="include some description about your page">  
         <title>Trivia Game</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
@@ -11,7 +11,7 @@
     <body>
         <div class="container" style="margin-top: 15px;">
             <div class="row col-xs-8">
-                <h1>CS4640 Television Trivia Game</h1>
+                <h1>Wordle Extreme Game</h1>
                 <h3>Hello <?=$user["name"]?>! Score: <?=$user["score"]?></h3>
             </div>
             <div class="row">
