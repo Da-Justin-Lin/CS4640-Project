7d4c4a7dc0503bd98f0c698190032495e0164d37
skych1 <sc4rdz@virginia.edu>
2022-03-23 18:24:11 -0400

    Update gameover.php

--- HW4/templates/gameover.php
+++ HW4/templates/gameover.php
@@ -12,21 +12,19 @@
         <div class="container" style="margin-top: 15px;">
             <div class="row col-xs-8">
                 <h1>Wordle Extreme Game</h1>
-                <h3>Game Over! Number of guesses: <?=$user["num_guess"]?></h3>
+                <h3>Hello <?=$user["name"]?>! Number of guesses: <?=$user["num_guess"]?></h3>
             </div>
             <div class="row">
                 <div class="col-xs-8 mx-auto">
                 <form action="?command=question" method="post">
                     <div class="h-100 p-5 bg-light border rounded-3">
-                    <h2>Previous number of guesses</h2>
-                    </div>
                     <?=$message?>
+                    </div>
                     <div class="h-10 p-5 mb-3">
-                        <input type="text" class="form-control" id="answer" name="answer" placeholder="Type your answer here">
                     </div>
                     <div class="text-center">                
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    <a href="?command=logout" class="btn btn-danger">End Game</a>
+                    <button type="submit" class="btn btn-primary">Play Again</button>
+                    <a href="?command=logout" class="btn btn-danger">Log out</a>
                     </div>
                 </form>
                 </div>
