1cf773fb26bc3090d9a5b732f58de4122f48ce09
skych1 <sc4rdz@virginia.edu>
2022-03-30 18:42:12 -0400

    hiostry

--- HW5/classes/financeController.php
+++ HW5/classes/financeController.php
@@ -20,7 +20,10 @@ class FinanceController {
                 break;
             case "logout":
                 $this->destroyCookies();
+                break;
             case "login":
+                $this->login();
+                break;
             case "new":
                 $this->new();
                 break;
@@ -35,15 +38,17 @@ class FinanceController {
     }
 
     private function login() {
+        session_start();
         if (isset($_POST["email"])) {
-            $data = $this->db->query("select * from user where email = ?;", "s", $_POST["email"]);
+            $data = $this->db->query("select * from hw5_user where email = ?;", "s", $_POST["email"]);
             if ($data === false) {
                 $error_msg = "Error checking for user";
             } else if (!empty($data)) {
                 if (password_verify($_POST["password"], $data[0]["password"])) {
                     setcookie("name", $data[0]["name"], time() + 3600);
                     setcookie("email", $data[0]["email"], time() + 3600);
-                    header("Location: ?command=question");
+                    $_SESSION["id"] = $data[0]["id"];
+                    header("Location: ?command=history");
                 } else {
                     $error_msg = "Wrong password";
                 }
@@ -60,8 +65,8 @@ class FinanceController {
                 } else {
                     setcookie("name", $_POST["name"], time() + 3600);
                     setcookie("email", $_POST["email"], time() + 3600);
-                    setcookie("score", 0, time() + 3600);
-                    header("Location: ?command=question");
+                    $_SESSION["id"] = $data[0]["id"];
+                    header("Location: ?command=history");
                 }
             }
         }
@@ -69,9 +74,9 @@ class FinanceController {
     }
 
     private function history() {
-        $data = $this->db->query("select * from hw5_transaction where user_id = ? order by t_date desc;");
-        $data2 = $this->db->query("select sum(amount) as balance from hw5_transaction where user_id = ?;");
-        $data3 = $this->db->query("select category, sum(amount) as balance from hw5_transaction group by category;");
+        $list = $this->db->query("select trans_name, trans_cat, t_date, amount, trans_type from hw5_transaction where user_id = ? order by t_date desc;");
+        $total_bal = $this->db->query("select sum(amount) as balance from hw5_transaction where user_id = ?;");
+        $cat_bal = $this->db->query("select trans_cat, sum(amount) where user_id = ? as balance from hw5_transaction group by category;");
         include("templates/history.php");
     }
 
@@ -79,15 +84,15 @@ class FinanceController {
         if (isset($_POST["trans_type"])) {
             // if credit, amount is positive
             if ($_POST["trans_type"] == "credit") {
-                $insert = $this->db->query("insert into hw5_transaction (name, trans_name, trans_cat, date, amount, trans_type) values (?, ?, ?, ?, ?, ?);", 
-                "ssssss", $_POST["name"], $_POST["trans_name"], $_POST["trans_cat"], 
-                $_POST["date"], $_POST["amount"], $_POST["trans_type"]);
+                $insert = $this->db->query("insert into hw5_transaction (name, trans_name, trans_cat, date, amount, trans_type, id) values (?, ?, ?, ?, ?, ?, ?);", 
+                "ssssss", $_COOKIE["name"], $_POST["trans_name"], $_POST["trans_cat"], 
+                $_POST["date"], $_POST["amount"], $_POST["trans_type"], $_SESSION["id"]);
             }
             // if debit, amount is negative
             else {
-                $insert = $this->db->query("insert into hw5_transaction (name, trans_name, trans_cat, date, amount, trans_type) values (?, ?, ?, ?, ?, ?);", 
-                "ssssss", $_POST["name"], $_POST["trans_name"], $_POST["trans_cat"], 
-                $_POST["date"], -1*$_POST["amount"], $_POST["trans_type"]);
+                $insert = $this->db->query("insert into hw5_transaction (name, trans_name, trans_cat, date, amount, trans_type, id) values (?, ?, ?, ?, ?, ?, ?);", 
+                "ssssss", $_COOKIE["name"], $_POST["trans_name"], $_POST["trans_cat"], 
+                $_POST["date"], -1*$_POST["amount"], $_POST["trans_type"], $_SESSION["id"]);
             }
         }
         if ($insert === false) {
